d9c5e3ecb0e828ec08c54d64bbb76d4ca868790c
Mehul Rawal <46857664+TheLast-King@users.noreply.github.com>
2026-08-26 17:09:35 +0530

    Add: sort-colors - submission-1

---
+++ Data Structures & Algorithms/sort-colors/submission-1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void sortColors(int[] nums) {
+      HashMap<Integer, Integer> mp = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            mp.put(nums[i], mp.getOrDefault(nums[i], 0) + 1);
+        }
+
+     int j =0;
+
+     for (int i = 0; i < nums.length; i++) {
+            while (mp.getOrDefault(j, 0) == 0) {
+                j++;
+            }
+
+            nums[i] = j;
+            mp.put(j, mp.get(j) - 1);
+        }
+    }
+}
